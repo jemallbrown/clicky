f748cdd9024ffbf720fec82c43959712005639f1
JayBee <jemallbrown@gmail.com>
2019-02-03 21:19:15 -0500

    adding setState functions for pushing to array and checking if picked character already

--- src/App.js
+++ src/App.js
@@ -3,48 +3,86 @@ import AvengerCard from "./components/AvengerCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import avengers from "./avengers.json";
+import Navbar from "./components/Navbar";
+
+// function mixthemup(array) {
+//   for (let i = array.length -1 ; i < array.length; i++) {
+//       let a = Math.floor(Math.random() * (i+1));
+//         [array[i], array[a]] = [array[a], array[i]];    
+//   }
+//   return array
+// }
+
 
 class App extends Component {
   // Setting this.state.avengers to the avengers json array
 
-
   state = {
     avengers,
-    pickedalready: false
+    clickedavenger: [],
+    score: 0
   };
 
-  avengerClicked = id => {
-    console.log(this.state.avengers[id-1].name)
-    console.log(this.state.avengers[id-1].pickedalready)
-    if (this.state.avengers[id-1].pickedalready === false) {
-      console.log('in the false leg of the if statement')
-      console.log(this.state.avengers[id-1].pickedalready);
-    //   this.addOne();
-      this.setState({ pickedalready: true });
-      console.log(this.state.pickedalready);
-    } else {
-    //   this.clickedtwice();
-    console.log('in the true leg of the if statement')
-    console.log(this.state.avengers[id-1].name,"has been clicked already");
-    }
+  avengerClicked = event => {
+    const currentavenger = event.target.alt
+    console.log('this is the current avenger -',currentavenger)
+
+    const alreadyclicked = (this.state.clickedavenger.indexOf(currentavenger) > -1);
+    console.log('already clicked is currently ',alreadyclicked)
+    console.log('this is the array before setState change',this.state.clickedavenger)
+
+    console.log(this.state.clickedavenger.indexOf(currentavenger))
+      
+      if (alreadyclicked) {
+        console.log("that avenger already clicked on")
+        this.setState({
+          clickedavenger: []
+        });
+      } else {
+        this.setState(
+          {
+            clickedavenger: this.state.clickedavenger.push(
+              currentavenger
+            )
+          }
+        )
+      }
+
+    console.log('this is array after setState change',this.state.clickedavenger)
+    // console.log(this.state.avengers[id-1].pickedalready)
+    // if (this.state.avengers[id-1].pickedalready === false) {
+    //   console.log('in the false leg of the if statement')
+    //   console.log('originaly pickedalready is ',this.state.avengers[id-1].pickedalready);
+    // } else {
+    // //   this.clickedtwice();
+    // console.log('in the true leg of the if statement')
+    // console.log(this.state.avengers[id-1].name,"has been clicked already");
+    // }
   };
+
+
 
   // Map over this.state.avengers and render a avengerCard component for each avenger object
   render() {
     return (
-      <Wrapper>
+      <div>
+      <Navbar
+        score={this.state.score}
+        />
+      
         <Title>Welcome to the Avenger Clicky Game</Title>
+      <Wrapper>
         {this.state.avengers.map(avenger => (
           <AvengerCard
             avengerClicked={this.avengerClicked}
             id={avenger.id}
             key={avenger.id}
-            name={avenger.name}
             image={avenger.image}
-            mixthemup={this.mixthemup}
+            name={avenger.name}
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 }
--- src/components/AvengerCard/index.js
+++ src/components/AvengerCard/index.js
@@ -4,26 +4,9 @@ import "./style.css";
 function AvengerCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
-        <img  onClick={() => props.avengerClicked(props.id)}
-        alt={props.name} src={props.image} />
+      <div onClick={(props.avengerClicked)} className="img-container">
+        <img alt={props.name} src={props.image} />
       </div>
-      {/* <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div> */}
-      {/* <span onClick={() => props.avengerClicked(props.id)} className="remove">
-        𝘅
-      </span> */}
     </div>
   );
 }
